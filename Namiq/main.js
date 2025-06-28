343d9ccbc571462fb5d1ad6e6d3f29dd33689c12
Namig <namig.pashayev1@gmail.com>
2025-06-28 21:58:58 +0400

    Add slideshow logic to heading section

---
+++ Namiq/main.js
@@ -0,0 +1,41 @@
+// header slideshow logic
+
+const headerSSImages = document.querySelectorAll(".heading-slideshow-cnr img")
+const headerSSPaginationDots = document.querySelectorAll(".heading-slideshow-cnr .pagination-dot")
+const headerSSPlantCategory = document.querySelectorAll(".heading-slideshow-cnr .plant-category")
+const headerSSPlantName = document.querySelectorAll(".heading-slideshow-cnr .plant-name")
+
+const headerSSNextButton = document.querySelector(".heading-slideshow-cnr .right-arrow-icon")
+
+let current = 0
+
+function showImage(current) {
+    headerSSImages.forEach((image, i) => {
+        image.style.display = i === current ? "block" : "none"
+    })
+
+    headerSSPaginationDots.forEach((dot, i) => {
+        dot.style.width = i === current ? "2.1rem" : "0.6rem"
+        dot.style.borderRadius = i === current ? "1000px" : "50%"
+    })
+
+    headerSSPlantCategory.forEach((category, i) => {
+        category.style.display = i === current ? "block" : "none"
+    })
+
+    headerSSPlantName.forEach((name, i) => {
+        name.style.display = i === current ? "block" : "none"
+    })
+}
+
+showImage(0)
+
+setInterval(() => {
+    current = (current + 1) % headerSSImages.length
+    showImage(current)
+}, 5000)
+
+headerSSNextButton.addEventListener("click", () => {
+    current = (current + 1) % headerSSImages.length;
+    showImage(current);
+});
