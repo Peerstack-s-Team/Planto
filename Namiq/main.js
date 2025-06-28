1da97552438b5cb2298e20b60c2e6058230f9437
Namig <namig.pashayev1@gmail.com>
2025-06-28 22:15:35 +0400

    fix: reset slideshow timer when user changes slide manually

--- Namiq/main.js
+++ Namiq/main.js
@@ -1,13 +1,11 @@
-// header slideshow logic
-
 const headerSSImages = document.querySelectorAll(".heading-slideshow-cnr img")
 const headerSSPaginationDots = document.querySelectorAll(".heading-slideshow-cnr .pagination-dot")
 const headerSSPlantCategory = document.querySelectorAll(".heading-slideshow-cnr .plant-category")
 const headerSSPlantName = document.querySelectorAll(".heading-slideshow-cnr .plant-name")
-
 const headerSSNextButton = document.querySelector(".heading-slideshow-cnr .right-arrow-icon")
 
 let current = 0
+let intervalId // to store interval reference
 
 function showImage(current) {
     headerSSImages.forEach((image, i) => {
@@ -28,14 +26,23 @@ function showImage(current) {
     })
 }
 
-showImage(0)
+function startSlideshow() {
+    intervalId = setInterval(() => {
+        current = (current + 1) % headerSSImages.length
+        showImage(current)
+    }, 5000)
+}
 
-setInterval(() => {
-    current = (current + 1) % headerSSImages.length
-    showImage(current)
-}, 5000)
+function resetSlideshowTimer() {
+    clearInterval(intervalId)
+    startSlideshow()
+}
+
+showImage(current)
+startSlideshow()
 
 headerSSNextButton.addEventListener("click", () => {
-    current = (current + 1) % headerSSImages.length;
-    showImage(current);
-});
+    current = (current + 1) % headerSSImages.length
+    showImage(current)
+    resetSlideshowTimer()
+})
