5fbbb9a890db6fd8d87773c09a47a5cbed9e985b
Namig <namig.pashayev1@gmail.com>
2025-07-04 12:42:27 +0400

    Add our best 02 section and give main styling and plant changer logic

--- Namiq/main.js
+++ Namiq/main.js
@@ -1,65 +1,109 @@
-const headerSSImages = document.querySelectorAll(".heading-slideshow-cnr img")
-const headerSSPaginationDots = document.querySelectorAll(".heading-slideshow-cnr .pagination-dot")
-const headerSSPlantCategory = document.querySelectorAll(".heading-slideshow-cnr .plant-category")
-const headerSSPlantName = document.querySelectorAll(".heading-slideshow-cnr .plant-name")
-const headerSSNextButton = document.querySelector(".heading-slideshow-cnr .right-arrow-icon")
+// const headerSSImages = document.querySelectorAll(".heading-slideshow-cnr img")
+// const headerSSPaginationDots = document.querySelectorAll(".heading-slideshow-cnr .pagination-dot")
+// const headerSSPlantCategory = document.querySelectorAll(".heading-slideshow-cnr .plant-category")
+// const headerSSPlantName = document.querySelectorAll(".heading-slideshow-cnr .plant-name")
+// const headerSSNextButton = document.querySelector(".heading-slideshow-cnr .right-arrow-icon")
 
-let current = 0
-let intervalId // to store interval reference
+// let currentSlideIndex = 0
+// let intervalId // to store interval reference
 
-function showImage(current) {
-    headerSSImages.forEach((image, i) => {
-        image.style.display = i === current ? "block" : "none"
-    })
+// function showImage(currentSlideIndex) {
+//     headerSSImages.forEach((image, i) => {
+//         image.style.display = i === currentSlideIndex ? "block" : "none"
+//     })
 
-    headerSSPaginationDots.forEach((dot, i) => {
-        dot.style.width = i === current ? "2.1rem" : "0.6rem"
-        dot.style.borderRadius = i === current ? "1000px" : "50%"
-    })
+//     headerSSPaginationDots.forEach((dot, i) => {
+//         dot.style.width = i === currentSlideIndex ? "2.1rem" : "0.6rem"
+//         dot.style.borderRadius = i === currentSlideIndex ? "1000px" : "50%"
+//     })
 
-    headerSSPlantCategory.forEach((category, i) => {
-        category.style.display = i === current ? "block" : "none"
-    })
+//     headerSSPlantCategory.forEach((category, i) => {
+//         category.style.display = i === currentSlideIndex ? "block" : "none"
+//     })
 
-    headerSSPlantName.forEach((name, i) => {
-        name.style.display = i === current ? "block" : "none"
-    })
+//     headerSSPlantName.forEach((name, i) => {
+//         name.style.display = i === currentSlideIndex ? "block" : "none"
+//     })
+// }
+
+// function startSlideshow() {
+//     intervalId = setInterval(() => {
+//         currentSlideIndex = (currentSlideIndex + 1) % headerSSImages.length
+//         showImage(currentSlideIndex)
+//     }, 5000)
+// }
+
+// function resetSlideshowTimer() {
+//     clearInterval(intervalId)
+//     startSlideshow()
+// }
+
+// showImage(currentSlideIndex)
+// startSlideshow()
+
+// headerSSNextButton.addEventListener("click", () => {
+//     currentSlideIndex = (currentSlideIndex + 1) % headerSSImages.length
+//     showImage(currentSlideIndex)
+//     resetSlideshowTimer()
+// })
+
+
+// // basket related logic
+
+// const addedItemsContainer = document.querySelector(".added-items-count");
+// const basketButtons = document.querySelectorAll(".basket-btn");
+
+// addedItemsContainer.textContent = 0;
+
+// basketButtons.forEach((button) => {
+//     button.addEventListener("click", () => {
+//         button.classList.toggle("added");
+
+//         const addedItemsCount = document.querySelectorAll(".basket-btn.added").length;
+//         addedItemsContainer.textContent = addedItemsCount;
+//     });
+// });
+
+
+// Best o2 section related logic
+
+const bestO2PaginationDots = document.querySelectorAll(".bo2-pagination-cnr .pagination-dot");
+const prevButton = document.querySelector(".prev-btn")
+const nextButton = document.querySelector(".next-btn")
+const plantNumberCnr = document.querySelector(".plant-number-cnr")
+const plantImages = document.querySelectorAll(".bo2-main img")
+console.log(plantImages)
+let currentPlantIndex = 0;
+
+function generatePlantNumber(currentPlantIndex) {
+    // padstart function adds "0" before number until it reaches two digit length 
+    plantNumberCnr.textContent = `${String(currentPlantIndex + 1).padStart(2, "0")}` + "/04"
 }
 
-function startSlideshow() {
-    intervalId = setInterval(() => {
-        current = (current + 1) % headerSSImages.length
-        showImage(current)
-    }, 5000)
-}
-
-function resetSlideshowTimer() {
-    clearInterval(intervalId)
-    startSlideshow()
-}
-
-showImage(current)
-startSlideshow()
-
-headerSSNextButton.addEventListener("click", () => {
-    current = (current + 1) % headerSSImages.length
-    showImage(current)
-    resetSlideshowTimer()
+prevButton.addEventListener('click', () => {
+    currentPlantIndex = currentPlantIndex === 0 ? 3 : currentPlantIndex - 1
+    changePlant(currentPlantIndex)
+    generatePlantNumber(currentPlantIndex)
 })
 
+nextButton.addEventListener('click', () => {
+    currentPlantIndex = currentPlantIndex === 3 ? 0 : currentPlantIndex + 1
+    changePlant(currentPlantIndex)
+    generatePlantNumber(currentPlantIndex)
+})
 
-// basket related logic
+function changePlant(currentPlantIndex) {
 
-const addedItemsContainer = document.querySelector(".added-items-count");
-const basketButtons = document.querySelectorAll(".basket-btn");
+    plantImages.forEach((image, i) => {
+        image.style.display = i === currentPlantIndex ? "block" : "none"
+    })
 
-addedItemsContainer.textContent = 0;
+    bestO2PaginationDots.forEach((dot, i) => {
+        dot.style.width = i === currentPlantIndex ? "3.1rem" : "1.1rem";
+        dot.style.borderRadius = i === currentPlantIndex ? "1000px" : "50%";
+    })
+}
 
-basketButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        button.classList.toggle("added");
+changePlant(currentPlantIndex)
+generatePlantNumber(currentPlantIndex)
 
-        const addedItemsCount = document.querySelectorAll(".basket-btn.added").length;
-        addedItemsContainer.textContent = addedItemsCount;
-    });
-});
