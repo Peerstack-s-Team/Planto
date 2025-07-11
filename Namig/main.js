70308a6f1a16098f1134e9c7e852769ef694f4c0
Namig <namig.pashayev1@gmail.com>
2025-07-11 20:42:07 +0400

    Add heading section

---
+++ Namig/main.js
@@ -0,0 +1,50 @@
+// Slideshow related logic
+
+const headerSSImages = document.querySelectorAll(".heading-slideshow-cnr img")
+const headerSSPaginationDots = document.querySelectorAll(".heading-slideshow-cnr .pagination-dot")
+const headerSSPlantCategory = document.querySelectorAll(".heading-slideshow-cnr .plant-category")
+const headerSSPlantName = document.querySelectorAll(".heading-slideshow-cnr .plant-name")
+const headerSSNextButton = document.querySelector(".heading-slideshow-cnr .right-arrow-icon")
+
+let currentSlideIndex = 0
+let intervalId // to store interval reference
+
+function showImage(currentSlideIndex) {
+    headerSSImages.forEach((image, i) => {
+        image.style.display = i === currentSlideIndex ? "block" : "none"
+    })
+
+    headerSSPaginationDots.forEach((dot, i) => {
+        dot.style.width = i === currentSlideIndex ? "2.1rem" : "0.6rem"
+        dot.style.borderRadius = i === currentSlideIndex ? "1000px" : "50%"
+    })
+
+    headerSSPlantCategory.forEach((category, i) => {
+        category.style.display = i === currentSlideIndex ? "block" : "none"
+    })
+
+    headerSSPlantName.forEach((name, i) => {
+        name.style.display = i === currentSlideIndex ? "block" : "none"
+    })
+}
+
+function startSlideshow() {
+    intervalId = setInterval(() => {
+        currentSlideIndex = (currentSlideIndex + 1) % headerSSImages.length
+        showImage(currentSlideIndex)
+    }, 5000)
+}
+
+function resetSlideshowTimer() {
+    clearInterval(intervalId)
+    startSlideshow()
+}
+
+showImage(currentSlideIndex)
+startSlideshow()
+
+headerSSNextButton.addEventListener("click", () => {
+    currentSlideIndex = (currentSlideIndex + 1) % headerSSImages.length
+    showImage(currentSlideIndex)
+    resetSlideshowTimer()
+})
